ba8db03468a6b7968781d90c5fa49a40d4b4d137
baguakiwork <41285682+baguakiwork@users.noreply.github.com>
2018-10-09 17:48:14 +0300

    werwer
    werwerwerwe

--- testjs3/JS/script.js
+++ testjs3/JS/script.js
@@ -1,1 +1,42 @@
-document.write("Hello world");
+const redFruits = ['strawberry', 'apple', 'cherry', 'cranberries'];
+function test(fruit, quantity) {
+    
+    //if (!fruit) throw('No fruits');
+    if (!fruit) document.write('No fruits');
+    if (!quantity) throw('No quantity');
+    if (redFruits.includes(fruit)) {
+        document.write('red' + '<br/>');
+    }
+    if (quantity > 10) {
+        document.write('quantity' + '>=' + quantity);
+    }
+
+
+}
+test();
+
+//document.write("Hello world");
+
+
+
+
+
+
+    /*       if (fruit == 'apple' || fruit == 'srawberry') {
+        console.log('red')
+    } 
+    if (redFruits.includes(fruit)){
+        console.log('red');
+    }
+    else console.log('Who knows'); */
+
+/* if (fruit) {
+    if (redFruits.includes(fruit)) {
+        console.log('red');
+        if (quantity > 10) {
+            console.log('big quantity');
+        }
+    } else {
+        console.log('No fruit');
+        }
+}  */

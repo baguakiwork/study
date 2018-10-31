d8da19abebbde30815ac37716e0a0baf54195ec2
baguakiwork <41285682+baguakiwork@users.noreply.github.com>
2018-10-31 17:05:30 +0200

    Update testjs3/JS/script.js

--- testjs3/JS/script.js
+++ testjs3/JS/script.js
@@ -1,51 +1,73 @@
-function greeting(){
-    document.write("<br/>" + "Hellow world");
-}
-greeting.lang = "English";
-greeting();
-document.write("<br/>" + greeting.lang);
+const arr1 = [1,2,5 ,3];
+const arr2 = [];
 
-const square = function(x) {
-    return x*x;
-}
-const qoob = square;
-
-document.write("<br/>" + square(8) + "   " +  qoob(5));
-
-function formalGreeting() {
-    document.write("<br/>" + "How are you?");
+function doubleArr(arr1) {
+    return arr2;
 }
 
-function casualGreeting() {
-    document.write("<br/>" + "What's up?");
+function logArrayElement(element, index, array){
+    document.write('a[' + index + '] = ' + element  +  "<br/>");
 }
 
-function greet(type, formalGreeting, casualGreeting) {
-    if (type === 'formal') {
-        formalGreeting();
-    } else if (type === 'casual') {
-        casualGreeting();
-    }
+
+for(let i = 0; i < arr1.length; i++) {
+    arr2.push(arr1[i]*2); 
 }
 
-greet('formal', formalGreeting, casualGreeting);
+document.write("<br/>" + arr2 + "<br/>");
+arr1.forEach(logArrayElement);
 
 
-const redFruits = ['strawberry', 'apple', 'cherry', 'cranberries'];
-function test(fruit, quantity) {
+
+
+// function greeting(){
+//     document.write("<br/>" + "Hellow world");
+// }
+// greeting.lang = "English";
+// greeting();
+// document.write("<br/>" + greeting.lang);
+
+// const square = function(x) {
+//     return x*x;
+// }
+// const qoob = square;
+
+// document.write("<br/>" + square(8) + "   " +  qoob(5));
+
+// function formalGreeting() {
+//     document.write("<br/>" + "How are you?");
+// }
+
+// function casualGreeting() {
+//     document.write("<br/>" + "What's up?");
+// }
+
+// function greet(type, formalGreeting, casualGreeting) {
+//     if (type === 'formal') {
+//         formalGreeting();
+//     } else if (type === 'casual') {
+//         casualGreeting();
+//     }
+// }
+
+// greet('formal', formalGreeting, casualGreeting);
+
+
+// const redFruits = ['strawberry', 'apple', 'cherry', 'cranberries'];
+// function test(fruit, quantity) {
     
-    //if (!fruit) throw('No fruits');
-    if (!fruit) document.write("<br/>" + 'No fruits');
-    if (!quantity) throw("<br/>" + 'No quantity');
-    if (redFruits.includes(fruit)) {
-        document.write('red' + '<br/>');
-    }
-    if (quantity > 10) {
-        document.write("<br/>" +  'quantity' + '>=' + quantity);
-    }
+//     //if (!fruit) throw('No fruits');
+//     if (!fruit) document.write("<br/>" + 'No fruits');
+//     if (!quantity) throw("<br/>" + 'No quantity');
+//     if (redFruits.includes(fruit)) {
+//         document.write('red' + '<br/>');
+//     }
+//     if (quantity > 10) {
+//         document.write("<br/>" +  'quantity' + '>=' + quantity);
+//     }
 
-}
-test();
+// }
+// test();
 
 //document.write("Hello world");
 

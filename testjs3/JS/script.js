33953bfbb136e8d775e902cf5e11d25304d8cbf8
baguakiwork <41285682+baguakiwork@users.noreply.github.com>
2018-11-09 15:23:13 +0200

    Update testjs3/JS/script.js

--- testjs3/JS/script.js
+++ testjs3/JS/script.js
@@ -1,33 +1,69 @@
-const arr1 = [1,2,5 ,10];
-const arr2 = [];
-
-function doubleArr(arr1) {
-    return arr2;
-}
-
-function logArrayElement(element, index, array){
-    document.write('a[' + index + '] = ' + element  +  "<br/>");
-}
-
-function DoubleArrayElement(element, index, array){
-    return array[index] = element + element; 
-}
+const foo = {a:"abc"};
+const bar = foo;
+const o1 = {a:0, b:[1, 2, 3], c:2};
+o1.a = 10;
+const o2 = {...o1};
+o2.a = 5;
 
 
-for(let i = 0; i < arr1.length; i++) {
-    arr2.push(arr1[i]*2); 
-}
+bar.a = "opera";
+bar.b = "b";
+
+document.write("<br/>", foo.a, "&nbsp", foo.b);
+document.write("<br/>", bar.a, "&nbsp", bar.b);
+document.write("<br/>", o1.a, "&nbsp", o1.b.slice(1,3));
+document.write("<br/>", o2.a, "&nbsp", o2.b.slice(1,3));
+
+
+
+
+// const input = new Uint8Array(1024*1024);
+
+// const copy = (input) => {return [...input]};
+// const copy2 = (input) => {input.map(num=>num+3)};
+
+// let time = performance.now();
+// const output = copy2(input);
+// time = performance.now() - time;
+// document.write('time is in micro = ', time);
+// document.write("<br/>", input)
+
+
+//console.log(output);
+
+
+// const arr1 = [1,2,5 ,10];
+// const arr2 = [];
+
+// function doubleArr(arr1) {
+//     return arr2;
+// }
+
+// function logArrayElement(element, index, array){
+//     document.write('a[' + index + '] = ' + element  +  "<br/>");
+// }
+
+// function DoubleArrayElement(element, index, array){
+//     return array[index] = element + element; 
+// }
+
+
+// for(let i = 0; i < arr1.length; i++) {
+//     arr2.push(arr1[i]*2); 
+// }
 
 
 // arr1.forEach(logArrayElement);
 // arr1.forEach(DoubleArrayElement);
-const arr3 = arr1.map(DoubleArrayElement);
+
+// const arr4 = [...arr1];
+// //const arr3 = arr1.map(DoubleArrayElement);
 
 
-document.write("<br/>" + arr1 + "<br/>");
-document.write("<br/>" + arr3 + "<br/>");
-
-document.write("hello");
+// //document.write("<br/>" + arr1 + "<br/>");
+// //document.write("<br/>" + arr3 + "<br/>");
+// document.write("<br/>" + arr4 + "<br/>");
+// document.write("hello");
 
 
 
